7a561008f12bd79e279c41817b4373106765f162
Cesar McFarlin <cesarmcfarlin@Cesars-MacBook-Pro.local>
2018-07-29 23:50:09 -0600

    liri fix
    function adjust

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'cvO78dtEClgYvIqGUTnZKuSLR',
+  consumer_secret: 'bEp2sU3yqESN35V6TtsnJKtXRFRlJ1ftvr5xho8IbIiw8kV5ab',
+  access_token_key: '18409050-M8YW5AkDdw6csXyV2ljE9WQH8zrtcg0GV1VYep92u',
+  access_token_secret: 'A1D66lIFlBeZOG2vR5rSBQ0Yk49xlqTEORKttvN1KngjU',
+}
--- liri.js
+++ liri.js
@@ -1,21 +1,151 @@
-// Read and set environment variables
-require("dotenv").config();
+//captures the user input on the command line
+var input = process.argv[2];
 
-// prints `SZzsigw3iqE0pSADB` to the console
-console.log(process.env.TWITTER_CONSUMER_KEY) 
-
-// prints `AMw4tYFP7an0lIul49C21aS9fk2cWZTGS8XVE` to the console
-console.log(process.env.TWITTER_CONSUMER_SECRET)
-// etc.
-
-//Grab data from keys.js
 var keys = require('./keys.js');
-var request = require('request');
 var twitter = require('twitter');
-var spotify = require('spotify');
+var parameters = {screen_name: 'camcfarlin', count: 20};
 var client = new twitter(keys.twitterKeys);
+
+//variables for spotify
+var spotify = require('spotify');
+var song = '';
+
+//variable for omdb
+var request = require('request');
+var movie = '';
+
+//variable for fs
 var fs = require('fs');
 
-//Stored argument's array
-var nodeArgv = process.argv;
-var command = process.argv[2];
+// if(input == 'do-what-it-says'){
+// 	input = 
+// }
+
+//TWITTER==============================================================================
+//if user input is my-tweets this will post the text only from my last 20 tweets
+if(input == 'my-tweets'){
+	console.log('');
+	console.log('-----------------------------------------------------------------------');
+	client.get('statuses/user_timeline', parameters, function(err, tweets, response){
+		if(!err){
+			for(i=19;i>=0;i--)
+			console.log('Tweet #' + (20 - i) + ': ' + tweets[i].text);
+		}
+	})
+}
+//SPOTIFY===============================================================================
+else if(input =='spotify-this-song'){
+	for(i=3;i<process.argv.length;i++){
+		var song = song + '+' + process.argv[i];
+	}
+	if(song == ''){
+		var song = 'whats+my+age+again';
+	}
+
+	spotify.search({type: 'track', query: song}, function(err,data){
+		if(!err){
+			console.log('--------------------------------------------------------------');
+			console.log('');
+			console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+			console.log('Album Name: ' + data.tracks.items[0].album.name);
+			console.log('Song Name: ' + data.tracks.items[0].name);
+			console.log('Preview Link: ' + data.tracks.items[0].preview_url);
+			console.log('');
+			console.log('---------------------------------------------------------------');
+		}
+	})
+}
+//OMDB===================================================================================
+else if(input == 'movie-this'){
+
+	for(i=3;i<process.argv.length;i++){
+		var movie = movie + '+' + process.argv[i];
+	}
+
+	request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json', function (error, response, body) {
+
+		if (!error && response.statusCode == 200) {
+			console.log('');
+			console.log('----------------------------------------------------------------');
+			console.log('Title: ' + JSON.parse(body)['Title']);
+			console.log('IMDB Rating: ' + JSON.parse(body)['imdbRating']);
+			console.log('');
+			console.log('Plot: ' + JSON.parse(body)['Plot']);
+			console.log('');
+			console.log('Actors: ' + JSON.parse(body)['Actors']);
+			console.log('');
+			console.log('Year: ' + JSON.parse(body)['Year']);
+			console.log('Runtime: ' + JSON.parse(body)['Runtime']);
+			console.log('Rated: ' + JSON.parse(body)['Rated']);
+			console.log('');
+			console.log('----------------------------------------------------------------');
+		}
+	});
+}
+//DO WHAT IT SAYS============================================================================
+else if(input == 'do-what-it-says'){
+	fs.readFile('random.txt', 'utf8', function(error, data){
+		var randomArray = data.split(' ');
+		var command = randomArray[0];
+		var media = '';
+
+		for(i=1;i<randomArray.length;i++){
+		media = media + '+' + randomArray[i];
+		};
+		if(command == 'my-tweets'){
+			console.log('');
+			console.log('-----------------------------------------------------------------------');
+			client.get('statuses/user_timeline', parameters, function(err, tweets, response){
+				if(!err){
+					for(i=19;i>=0;i--)
+					console.log('Tweet #' + (20 - i) + ': ' + tweets[i].text);
+				}
+			})
+		}
+		else if(command =='spotify-this-song'){
+			var song = media;
+			
+			if(song == '+'){
+				var song = 'whats+my+age+again';
+			}
+
+			spotify.search({type: 'track', query: song}, function(err,data){
+				if(!err){
+					console.log('--------------------------------------------------------------');
+					console.log('');
+					console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+					console.log('Album Name: ' + data.tracks.items[0].album.name);
+					console.log('Song Name: ' + data.tracks.items[0].name);
+					console.log('Preview Link: ' + data.tracks.items[0].preview_url);
+					console.log('');
+					console.log('---------------------------------------------------------------');
+				}
+			})
+		}
+		else if(command == 'movie-this'){
+			var movie = media;
+
+			request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json', function (error, response, body) {
+
+				if (!error && response.statusCode == 200) {
+					console.log('');
+					console.log('----------------------------------------------------------------');
+					console.log('Title: ' + JSON.parse(body)['Title']);
+					console.log('IMDB Rating: ' + JSON.parse(body)['imdbRating']);
+					console.log('');
+					console.log('Plot: ' + JSON.parse(body)['Plot']);
+					console.log('');
+					console.log('Actors: ' + JSON.parse(body)['Actors']);
+					console.log('');
+					console.log('Year: ' + JSON.parse(body)['Year']);
+					console.log('Runtime: ' + JSON.parse(body)['Runtime']);
+					console.log('Rated: ' + JSON.parse(body)['Rated']);
+					console.log('');
+					console.log('----------------------------------------------------------------');
+				}
+			});
+		}	
+	})
+};
+
+
